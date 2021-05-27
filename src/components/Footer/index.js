0d58814de868f6526ff9a3323424548e52f65f42
Florin <florin@dnvn.co>
2021-05-26 21:08:35 -0700

    Footer year change

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -22,7 +22,7 @@ const Footer = () => (
           Site Footer
         </ScreenReaderText>
         <Link to="/" aria-label="Home page" rel="home">
-          &copy; 2020–{currentYear} Florin Catalin
+          &copy; {currentYear} Florin Catalin
         </Link>
         <SocialLinksWrap aria-labelledby="social-links-label">
           <ScreenReaderText>
